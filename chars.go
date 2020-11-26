b23a8017338f4353a854161a6b8b2c05683a127d
LAPTOP-0OLFV40E\28134 <2813474070@qq.com>
2020-11-26 17:29:46 +0800

    数据类型转换

---
+++ chars.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vgmdj/utils/logger"
+	"math"
+	"strconv"
+	"strings"
+)
+
+//ToInt 转换成int格式
+func ToInt(num interface{}) int {
+	switch num.(type) {
+	default:
+		logger.Error("invalid type ", num)
+		return 0
+
+	case string:
+		str := num.(string)
+		index := strings.Index(str, ".")
+		if index != -1 {
+			str = str[:index]
+		}
+
+		result, _ := strconv.Atoi(str)
+		return result
+
+	case int:
+		return num.(int)
+
+	case int32:
+		return int(num.(int32))
+
+	case int64:
+		return int(num.(int64))
+
+	case float64:
+		return int(num.(float64))
+
+	case bool:
+		if num.(bool) {
+			return 1
+		}
+		return 0
+
+	case []byte:
+		result := 0
+		for k, v := range num.([]byte) {
+			result += (int(v) - 48) * int(math.Pow10(len(num.([]byte))-k-1))
+		}
+		return result
+
+	case nil:
+		return 0
+
+	}
+}
+
+//ToFloat64 转换成float64格式
+func ToFloat64(num interface{}) float64 {
+	switch num.(type) {
+	default:
+		logger.Error("invalid type ", num)
+		return 0
+
+	case float64:
+		return num.(float64)
+
+	case string:
+		result, _ := strconv.ParseFloat(num.(string), 64)
+		return result
+
+	case int:
+		return float64(num.(int))
+
+	case int32:
+		return float64(num.(int32))
+
+	case int64:
+		return float64(num.(int64))
+
+	case nil:
+		return 0
+
+	}
+}
+
+//ToString 转换成string
+func ToString(num interface{}, prec ...int) string {
+	var p = 2
+	if len(prec) != 0 {
+		p = prec[0]
+	}
+
+	switch num.(type) {
+	default:
+		logger.Error("invalid type ", num)
+		return ""
+
+	case string:
+		str := num.(string)
+		if len(prec) == 0 {
+			return str
+		}
+
+		str = strings.Trim(str, "0")
+		index := strings.Index(str, ".")
+		if index == -1 {
+			str += "."
+			index = len(str) - 1
+		}
+
+		str += addZero(p)
+
+		str = str[:index+p+1]
+
+		if str == "" || str[0] == '.' {
+			str = "0" + str
+		}
+
+		if str[len(str)-1] == '.' {
+			str = str[:len(str)-1]
+		}
+
+		return str
+
+	case int:
+		return strconv.Itoa(num.(int))
+
+	case int32:
+		return strconv.Itoa(int(num.(int32)))
+
+	case int64:
+		return strconv.FormatInt(num.(int64), 10)
+
+	case float64:
+		return strconv.FormatFloat(num.(float64), 'f', p, 64)
+
+	case nil:
+		return ""
+
+	}
+}
+
+
+func addZero(n int) string {
+	result := ""
+	for i := 0; i < n; i++ {
+		result += "0"
+	}
+
+	return result
+}
+
+// float保留两位小数
+func FloatRetain(f float64) float64  {
+	value, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", f), 64)
+
+	return value
+}
---
+++ chars_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vgmdj/utils/logger"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestToInt(t *testing.T) {
+	param := map[string]interface{}{}
+	param["int10"] = int(10)
+	param["int6410"] = int64(10)
+
+	param["float10.0"] = 10.0
+	param["float10.02"] = 10.02
+	param["float0.1"] = 0.1
+	param["float0.01"] = 0.01
+	param["float0.10"] = 0.10
+
+	param["string"] = ""
+	param["string0"] = "0"
+	param["string10"] = "10"
+	param["string10.0"] = "10.0"
+	param["string10.01"] = "10.01"
+	param["string0.1"] = "0.1"
+	param["string0.01"] = "0.01"
+	param["string0.10"] = "0.10"
+
+	for k, v := range param {
+		logger.Info(k, "ToInt", ToInt(v))
+		logger.Info(k, "ToFloat64", ToFloat64(v))
+		logger.Info(k, "ToString", ToString(v))
+	}
+}
+
+func TestFloatRetain(t *testing.T) {
+	num := 0.123456
+
+	value1, _ := strconv.ParseFloat(fmt.Sprintf("%.1f", num), 64)
+	logger.Info("11", value1)
+
+	value2, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", num), 64)
+	logger.Info("22", value2)
+
+	value3, _ := strconv.ParseFloat(fmt.Sprintf("%.3f", num), 64)
+	logger.Info("33", value3)
+
+	// 测试不通过
+	//value4, _ := strconv.ParseFloat(fmt.Sprintf("%.4f", num), 64)
+	//logger.Info("44", value4)
+
+}
+
+func TestToString(t *testing.T) {
+	// string to int
+	count, _ := strconv.Atoi("10")
+	logger.Info("string to int", count)
+
+	// string to float64
+	f, _ := strconv.ParseFloat("10.00", 64)
+	logger.Info("string to float64", f)
+
+	// int to string
+	str := strconv.Itoa(10)
+	logger.Info("int to string", str)
+
+	logger.Info("int to string", fmt.Sprintf("%v----%d", 10, 10))
+
+	// float to string
+	logger.Info("float to string", fmt.Sprintf("%.2f", 0.12))
+
+}
+
+func TestToString2(t *testing.T) {
+	now := time.Now()
+	logger.Info("time now", now)  // cst 中国沿海时间(北京时间)
+	strTime := "2020-11-26 11:26:00"
+
+	// string to time
+	timeT, _ := time.ParseInLocation("2006-01-02 15:04:05", strTime, time.Local) // cst 中国沿海时间(北京时间)
+    logger.Info("string to time", timeT)
+
+	timeT2, _ := time.Parse("2006-01-02 15:04:05", strTime)  // utc 世界协调时间(不要用这个，容易出错)
+	logger.Info("string to time", timeT2)
+
+	// time to string
+	logger.Info("time to string", now.Format("2006-01-02 15:04:05"))
+}
